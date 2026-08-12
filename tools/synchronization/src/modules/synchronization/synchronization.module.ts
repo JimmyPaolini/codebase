8f799a806d1026868af6f17ef3211bfa416738bd
Jimmy Paolini <jimmypaolini@gmail.com>
2026-08-11 22:19:59 -0400

    refactor(conformetry): ♻️ reorder devcontainer environment variables and remove redundant synchronization configuration

--- tools/synchronization/src/modules/synchronization/synchronization.module.ts
+++ tools/synchronization/src/modules/synchronization/synchronization.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
 
 import { AgentSkillsModule } from "../agent-skills/agent-skills.module";
 import { ConformanceGeneratorsModule } from "../conformance-generators/conformance-generators.module";
@@ -8,7 +7,6 @@ import { DevcontainerConfigurationModule } from "../devcontainer-configuration/d
 import { LoggerModule } from "../logger/logger.module";
 import { PullRequestTemplateModule } from "../pull-request-template/pull-request-template.module";
 
-import { environmentSchema } from "./synchronization.constants";
 import { SynchronizationService } from "./synchronization.service";
 
 /**
@@ -18,12 +16,6 @@ import { SynchronizationService } from "./synchronization.service";
   controllers: [],
   exports: [SynchronizationService],
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: ".env",
-      isGlobal: true,
-      validate: (config: Record<string, unknown>) =>
-        environmentSchema.parse(config),
-    }),
     LoggerModule,
     AgentSkillsModule,
     ConformanceGeneratorsModule,
